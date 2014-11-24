6662639e6ca5b76dd345fc5cbc6f084ed0748f0b
paschmann <paul@paups.com>
2014-11-24 11:36:14 -0500

    Update js/m2.sqlbuilder.js fixes #18

--- js/m2.sqlbuilder.js
+++ js/m2.sqlbuilder.js
@@ -1,29 +1,62 @@
-function showSQLBuilder(sql, datatype){
+function showSQLBuilder(sqlquery, datatype, odataquery, wsquery){
     var output = '';
+    var sqlqueryactive = '';
+    var odataqueryactive = '';
+    var wsqueryactive = '';
+    
+    if (sqlquery !== ''){
+        sqlqueryactive = 'active';
+    } else if (odataquery !== ''){
+        odataqueryactive = 'active';
+    } else if (wsquery !== ''){
+        wsqueryactive = 'active';
+    }
     
     output += "<div role='tabpanel'>";
 
-        output += "<ul class='nav nav-tabs' role='tablist'>";
-        output += "<li role='presentation' class='active'><a href='#SQL' aria-controls='home' role='tab' data-toggle='tab'>SQL</a></li>";
-        //output += "<li role='presentation'><a href='#OData' aria-controls='OData' role='tab' data-toggle='tab'>OData</a></li>";
-        //output += "<li role='presentation'><a href='#WS' aria-controls='WS' role='tab' data-toggle='tab'>Web Service</a></li>";
+        output += "<ul class='nav nav-tabs' id='tabs' role='tablist'>";
+        output += "<li role='presentation' class='" + sqlqueryactive + "'><a href='#SQL' aria-controls='home' role='tab' id='sqltab' data-toggle='tab'>SQL</a></li>";
+        output += "<li role='presentation' class='" + odataqueryactive + "'><a href='#OData' aria-controls='OData' id='odatatab'role='tab' data-toggle='tab'>OData</a></li>";
+        output += "<li role='presentation' class='" + wsqueryactive + "'><a href='#WS' aria-controls='WS' role='tab' id='wstab' data-toggle='tab'>Web Service</a></li>";
         output += "</ul>";
 
         output += "<div class='tab-content'>";
-            output += "<div role='tabpanel' class='tab-pane active' id='SQL'>";
+            output += "<div role='tabpanel' class='tab-pane " + sqlqueryactive + "' id='SQL'>";
                 output += "<form class='form-horizontal' role='form' style='margin-top: 30px;' id='SQL'>";
                     output += "<input type='" + debugmode + "' value='" + "" + "' id='dashboardwidgetid' />";
                     output += "<div class='form-group'><label class='col-md-1 control-label'>Data Type: </label><label id='datatype' class='col-md-1 control-label'>" + datatype + "</label></div>";
                     output += "<div class='form-group'><label class='col-md-1 control-label'>SQL Statement: </label>";
-                        output += "<div class='col-md-10'><textarea class='form-control' rows='3' id='txtSQL'>" + sql + "</textarea></div>"
+                        output += "<div class='col-md-10'><textarea class='form-control' rows='3' id='txtSQL'>" + sqlquery + "</textarea></div>"
                         output += "<div class='col-md-1'><button type='button' id='btnExecuteSQL' class='btn btn-success' style='margin-top: 20px;'>Execute</button></div>";
                     output += "</div>";
                     output += "<div id='dialogSQLMsg' class='col-md-10 col-md-offset-1'></div>";
                     output += "<div id='output'></div>";
                 output += "</form>";
             output += "</div>";
-            output += "<div role='tabpanel' class='tab-pane' id='OData'>OData</div>";
-            output += "<div role='tabpanel' class='tab-pane' id='WS'>Web Services</div>";
+            output += "<div role='tabpanel' class='tab-pane " + odataqueryactive + "' id='OData'>";
+                output += "<form class='form-horizontal' role='form' style='margin-top: 30px;' id='OData'>";
+                    output += "<input type='" + debugmode + "' value='" + "" + "' id='dashboardwidgetid' />";
+                    output += "<div class='form-group'><label class='col-md-1 control-label'>Data Type: </label><label id='datatype' class='col-md-1 control-label'>" + datatype + "</label></div>";
+                    output += "<div class='form-group'><label class='col-md-1 control-label'>OData Query: </label>";
+                        output += "<div class='col-md-10'><textarea class='form-control' rows='3' id='txtOData'>" + odataquery + "</textarea></div>"
+                        output += "<div class='col-md-1'><button type='button' id='btnExecuteOData' class='btn btn-success' style='margin-top: 20px;'>Execute</button></div>";
+                    output += "</div>";
+                    output += "<div id='dialogODataMsg' class='col-md-11 col-md-offset-1'></div>";
+                    output += "<div id='Odataoutput' style='overflow: auto; height: 600px;' class='col-md-11 col-md-offset-1'></div>";
+                output += "</form>";
+            output += "</div>";
+            output += "<div role='tabpanel' class='tab-pane " + wsqueryactive + "' id='WS'>";
+                output += "<form class='form-horizontal' role='form' style='margin-top: 30px;' id='WS'>";
+                    output += "<input type='" + debugmode + "' value='" + "" + "' id='dashboardwidgetid' />";
+                    output += "<div class='form-group'><label class='col-md-1 control-label'>Data Type: </label><label id='datatype' class='col-md-1 control-label'>" + datatype + "</label></div>";
+                    output += "<div class='form-group'><label class='col-md-1 control-label'>Web Service Query: </label>";
+                        output += "<div class='col-md-10'><textarea class='form-control' rows='3' id='txtWS'>" + wsquery + "</textarea></div>"
+                        output += "<div class='col-md-1'><button type='button' id='btnExecuteWS' class='btn btn-success' style='margin-top: 20px;'>Execute</button></div>";
+                    output += "</div>";
+                    output += "<div id='dialogWSMsg' class='col-md-11 col-md-offset-1'></div>";
+                    output += "<div id='WSoutput' style='overflow: auto; height: 600px;' class='col-md-11 col-md-offset-1'></div>";
+                output += "</form>";
+            output += "</div>";
         output += "</div>";
     output += "</div>";
     
@@ -34,7 +67,38 @@ function showSQLBuilder(sql, datatype){
     $('#modalsqldlg').css('height','auto');
     $('#modalsqldlg').css('width','85%');
     $('#mySQLBuilderModal').appendTo("body").modal('show');
+}
 
+function executeWSRequest(){
+    $.ajax({
+        type: "get",
+        async: false,
+        url: $('#txtWS').val(),
+        success: function (data) {
+            $("#WSoutput").JSONView(JSON.stringify(data));
+            $('#dialogWSMsg').empty();
+        },
+        error: function (xhr, textStatus, errorMessage) {
+            $('#dialogWSMsg').html("<div class='alert alert-warning' role='alert'>" + errorMessage + "</div>");
+        }
+    });
+    
+}
+
+function executeODataRequest(){
+    $.ajax({
+        type: "get",
+        async: false,
+        url: $('#txtOData').val(),
+        success: function (data) {
+            $("#Odataoutput").JSONView(JSON.stringify(data));
+            $('#dialogODataMsg').empty();
+        },
+        error: function (xhr, textStatus, errorMessage) {
+            $('#dialogODataMsg').html("<div class='alert alert-warning' role='alert'>" + errorMessage + "</div>");
+        }
+    });
+    
 }
 
 function showSQLResults(data){
@@ -70,7 +134,7 @@ function showSQLResults(data){
         
         $('#output').html(html);
         $('#dialogSQLMsg').html('');
-        $('#datatable').DataTable({bFilter: false, deferRender:    true, dom: "C<'clear'>frtiS", scrollY: 600, scrollCollapse: true, stateSave: true,  paginate: false, scrollCollapse: true});
+        $('#datatable').DataTable({bFilter: false, deferRender:    true, dom: "C<'clear'>frtiS", scrollY: 570, scrollCollapse: true, stateSave: true,  paginate: false, scrollCollapse: true});
     } catch (e) {
         $('#dialogSQLMsg').html("<div class='alert alert-warning' role='alert'>" + data + "</div>");
     }
