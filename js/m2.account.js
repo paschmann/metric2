3d70546cc42a6f847807e13fa1e7952839a637fa
paschmann <paul@paups.com>
2015-01-13 10:18:38 -0500

    Update js/m2.account.js fixes #72

--- js/m2.account.js
+++ js/m2.account.js
@@ -1,6 +1,11 @@
+function toggleForm(){
+    $('#login').toggle();
+    $('#create').toggle();
+}
+
 $(document).ready(function () {
     var CSRFToken = '';
-    
+
         $("#loginform").submit(function (e) {
             $.ajax({
             	url: "lib/api.xsjs",
@@ -32,22 +37,23 @@ $(document).ready(function () {
                 type: "POST",
                 data:{
                     service: "CreateUser",
-                    email: $('#email').val(),
-                    password: $('#password').val(),
-                    name: $('#name').val(),
-                    lname: $('#lname').val(),
-                    company: $('#company').val()
+                    email: $('#createemail').val(),
+                    password: $('#createpassword').val(),
+                    name: $('#createname').val(),
+                    lname: $('#createlname').val(),
+                    company: $('#createcompany').val()
                 },
                 success: function(data, textStatus, XMLHttpRequest) {
                 	if (data == '-1'){
-                	    $('#msg').html('Account already exists');
+                	    $('#createmsg').html('Account already exists');
                 	} else if (data == '0') {
-                	    $('#msg').html('Error creating the account');
+                	    $('#createmsg').html('Error creating the account');
                 	} else if (data == '1') {
-                	    window.location = 'login.html?created=true';
+                	    toggleForm();
+                	    $('#msg').html('Account created, please login');
                 	}
                 }, error: function(XMLHttpRequest, textStatus, errorThrown) { 
-                    $('#msg').html('Status: ' + textStatus + " Error: " + errorThrown);
+                    $('#createmsg').html('Status: ' + textStatus + " Error: " + errorThrown);
                 } 
             });
             return false;
