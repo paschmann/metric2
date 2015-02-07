b03257e087669b730a37fbc41b385d8089eb08a2
paschmann <paul@paups.com>
2015-02-06 21:09:01 -0500

    Update js/m2.functions.js fixes #76

--- js/m2.functions.js
+++ js/m2.functions.js
@@ -155,4 +155,20 @@ Date.prototype.format = function (mask, utc) {
 };
 
 
+function parseParams(val) {
+    var result = "Not found",
+        tmp = [];
+    location.search
+    //.replace ( "?", "" ) 
+    // this is better, there might be a question mark inside
+    .substr(1)
+        .split("&")
+        .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === val) result = decodeURIComponent(tmp[1]);
+    });
+    return result;
+}
+
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');  ga('create', 'UA-3043336-20', 'auto');  ga('send', 'pageview');
